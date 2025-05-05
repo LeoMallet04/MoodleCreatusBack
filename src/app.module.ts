e02f7301210bbd90c39f1565c603f7ad7263eedb
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-05 12:19:14 -0300

    refact: Inclusão do GitTokenModule nos modulos da app

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { CardsModule } from './cards/cards.module';
 import { MaterialModule } from './material/material.module';
 import { ProjectModule } from './project/project.module';
 import { AuthModule } from './auth/auth.module';
+import { GitTokenModule} from './git_token/git_token.module';
 
 @Module({
-  imports: [UserModule, EventModule, CardsModule, MaterialModule, ProjectModule, AuthModule],
+  imports: [UserModule, EventModule, CardsModule, MaterialModule, ProjectModule, AuthModule, GitTokenModule],
   controllers: [AppController],
   providers: [AppService],
 })
