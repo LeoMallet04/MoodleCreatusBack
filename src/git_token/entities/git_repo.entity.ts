cb7bd871a9ed00fed096b134754435b3c1dec640
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-05 15:06:45 -0300

    feat: Criação de uma entidade para guardar as informações dos repositórios

---
+++ src/git_token/entities/git_repo.entity.ts
@@ -0,0 +1,6 @@
+export class GitRepo {
+    repo_name: string; //Nome do repo
+    repo_description: string; // Descrição
+    repo_languages: string[]; // Lista de linguagens
+    repo_url: string; // URL do REPO
+}
