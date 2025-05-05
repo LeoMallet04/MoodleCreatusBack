d71d651e9fee90688d868cb88d17766590115b5c
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-05 15:05:56 -0300

    refact: Ajustes nos controllers relacionados à Git API

--- src/git_token/git_token.controller.ts
+++ src/git_token/git_token.controller.ts
@@ -2,8 +2,9 @@ import { Controller, Post, Get, UseGuards, Body } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { Roles } from 'src/auth/roles.decorator';
 import { GitTokenService } from './git_token.service';
+import { url } from 'inspector';
 
-@Controller('git-token')
+@Controller('git-api')
 export class GitTokenController {
     constructor( 
         private readonly gitTokenService: GitTokenService
@@ -16,10 +17,11 @@ export class GitTokenController {
       return this.gitTokenService.getGitTokenWithGitApp();
     }
 
+
     @UseGuards(AuthGuard)
-    @Roles('ADMIN','NORMAL')
-    @Post('access-personal-token')
-    postPersonalToken(){
-      return this.gitTokenService.getGitPersonalToken();
+    @Roles('ADMIN', 'NORMAL')
+    @Post('repos-auth')
+    postRepoAuth(@Body('url') url:string){
+      return this.gitTokenService.getRepoData(url);
     }
 }
